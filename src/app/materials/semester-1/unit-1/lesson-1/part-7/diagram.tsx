408c63bb4602da8c963248f73cd55dc99892d6b9
Hazim Jaber <h75jbr@gmail.com>
2025-08-14 16:04:30 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Diagram>
    
    with th

--- src/app/materials/semester-1/unit-1/lesson-1/part-7/diagram.tsx
+++ src/app/materials/semester-1/unit-1/lesson-1/part-7/diagram.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { cn } from "@/lib/utils.tsx";
@@ -13,7 +12,7 @@ function Diagram({ className, ...props }: SVGProps<SVGSVGElement>) {
                         <polygon points="0 0, 5 1.75, 0 3.5" fill="hsl(var(--muted-foreground))" />
                     </marker>
                 </defs>
-                <g transform="translate(0, -9)">
+                <g transform="translate(0, -4)">
                     <line x1="20" y1="110" x2="20" y2="10" stroke="hsl(var(--muted-foreground))" strokeWidth="1.5" markerEnd="url(#arrowhead-avogadro)" />
                     <text x="10" y="15" dominantBaseline="middle" textAnchor="middle" fontSize="12" fill="hsl(var(--foreground))" fontWeight="bold">V</text>
                     
