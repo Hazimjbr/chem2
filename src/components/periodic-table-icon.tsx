f77b81274e3e6e99b6a346da37b42a235b9ca471
Hazim Jaber <h75jbr@gmail.com>
2025-08-18 19:02:25 +0000

    قلل عدد الدورات من تحت 2

--- src/components/periodic-table-icon.tsx
+++ src/components/periodic-table-icon.tsx
@@ -15,18 +15,17 @@ function PeriodicTableIcon({ className, ...props }: SVGProps<SVGSVGElement>) {
       {...props}
     >
       {/* Main block */}
-      <rect x="2" y="6" width="20" height="14" rx="1" />
+      <rect x="2" y="6" width="20" height="10" rx="1" />
       
       {/* Top row lines */}
       <line x1="2" y1="10" x2="22" y2="10" />
       <line x1="2" y1="13" x2="22" y2="13" />
-      <line x1="2" y1="17" x2="22" y2="17" />
 
       {/* Vertical lines */}
-      <line x1="6" y1="6" x2="6" y2="20" />
-      <line x1="10" y1="6" x2="10" y2="20" />
-      <line x1="14" y1="6" x2="14" y2="20" />
-      <line x1="18" y1="6" x2="18" y2="20" />
+      <line x1="6" y1="6" x2="6" y2="16" />
+      <line x1="10" y1="6" x2="10" y2="16" />
+      <line x1="14" y1="6" x2="14" y2="16" />
+      <line x1="18" y1="6" x2="18" y2="16" />
 
       {/* Top left element (Hydrogen) */}
       <rect x="2" y="2" width="4" height="4" rx="0.5" />
