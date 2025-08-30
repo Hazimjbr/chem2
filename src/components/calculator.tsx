120e2b8a05b64260a9b5bbdc0d724b8c36adaaa4
Hazim Jaber <h75jbr@gmail.com>
2025-08-30 19:51:34 +0000

    رح نضل نعيد كمان زفت يومين
    Hazim
    في الوضع الافقي بس
    
    تعديل بنية البي

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -21,7 +21,7 @@ const buttons = [
 
 const landscapeButtons = [
     // Row 1
-    { display: '9', value: '9' }, { display: '8', value: '8' }, { display: '7', value: '7' }, { display: '(', value: '(' }, { display: ')', value: ')' }, { display: 'log', value: 'log' }, { display: 'ln', value: 'ln' },
+    { display: '9', value: '9' }, { display: '8', value: '8' }, { display: '7', value: '7' }, { display: '(', value: ')' }, { display: ')', value: '(' }, { display: 'log', value: 'log' }, { display: 'ln', value: 'ln' },
     // Row 2
     { display: '6', value: '6' }, { display: '5', value: '5' }, { display: '4', value: '4' }, { display: '*', value: '*' }, { display: '/', value: '/' }, { display: 'e', value: 'e' }, { display: 'tan', value: 'tan' },
     // Row 3
@@ -175,3 +175,5 @@ export default function Calculator() {
     </div>
   );
 }
+
+    
