9cc2379d250b65d2d23265fc7684ff5fc36d1ee2
Hazim Jaber <h75jbr@gmail.com>
2025-09-04 04:56:14 +0000

    اكتب في اعلى الخط Ea

--- src/app/materials/semester-1/unit-1/lesson-2/part-2/diagram.tsx
+++ src/app/materials/semester-1/unit-1/lesson-2/part-2/diagram.tsx
@@ -76,14 +76,15 @@ export default function MaxwellBoltzmannDiagram() {
         backgroundG.drawingContext.setLineDash([5, 5]);
         const lineBottomY = CANVAS_HEIGHT - 20;
         const lineTopY = (lineBottomY + 10) / 2;
-        backgroundG.line(startX_Ea, lineTopY - ((lineBottomY - lineTopY) * 0.25), startX_Ea, lineBottomY);
+        const finalLineTopY = lineTopY - ((lineBottomY - lineTopY) * 0.25);
+        backgroundG.line(startX_Ea, finalLineTopY, startX_Ea, lineBottomY);
         backgroundG.drawingContext.setLineDash([]); // Reset line dash
         
         // Draw Ea label
         backgroundG.noStroke();
         backgroundG.fill('hsl(var(--destructive))');
         backgroundG.textAlign(p.RIGHT);
-        backgroundG.text('Ea', startX_Ea - 5, 20);
+        backgroundG.text('Ea', startX_Ea - 5, finalLineTopY - 5);
 
         p.noLoop(); // Don't start drawing the curve until temperature is updated
       };
