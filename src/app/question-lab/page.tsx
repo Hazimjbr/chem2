8cb497025bfe2330c7a6b0d3df2d594cf97d43c0
Hazim Jaber <h75jbr@gmail.com>
2025-09-05 19:07:55 +0000

    بدي تزبط عرض درجة الحرارة المئوية بحيث تكون الوحدة على يمين الرقم مش يسا

--- src/app/question-lab/page.tsx
+++ src/app/question-lab/page.tsx
@@ -22,7 +22,7 @@ interface Question {
 const newQuestion: Question | null = {
     questionText: (
         <>
-            <p>اعتمادا على الرسم المجاور والذي يمثل العلاقة بين درجة الحرارة °C والضغط البخاري mmHg لأربعة سوائل فإن الضغط البخاري لثنائي إيثيل إيثر C₂H₅OC₂H₅ عند 30°C يساوي:</p>
+            <p>اعتمادا على الرسم المجاور والذي يمثل العلاقة بين درجة الحرارة °C والضغط البخاري mmHg لأربعة سوائل فإن الضغط البخاري لثنائي إيثيل إيثر C₂H₅OC₂H₅ عند <span dir="ltr" className="inline-block">30°C</span> يساوي:</p>
             <div className="flex justify-center my-4">
                 <Image
                     src="https://i.ibb.co/84T52SSm/3.png"
