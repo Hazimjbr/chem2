9699622b6bd4e0da32d61dc71547befb7d9d3975
Hazim Jaber <h75jbr@gmail.com>
2025-09-09 15:52:17 +0000

    جرب هذا الينك https://i.ibb.co/YTDHLDq9/image.jpg

--- src/app/materials/semester-1/unit-1/lesson-3/part-3/page.tsx
+++ src/app/materials/semester-1/unit-1/lesson-3/part-3/page.tsx
@@ -142,7 +142,7 @@ export default function LessonPartPage() {
                     <div className="flex flex-col items-center">
                         <h4 className="font-bold mb-2">الألماس</h4>
                          <Image
-                            src="https://i.ibb.co/68B1w2t/4.png"
+                            src="https://i.ibb.co/YTDHLDq9/image.jpg"
                             alt="بنية الألماس"
                             width={150}
                             height={150}
