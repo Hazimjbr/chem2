1957e9fe2870311724827844710c24ba6102d052
Hazim Jaber <h75jbr@gmail.com>
2025-09-11 08:44:40 +0000

    بدل الصورة بهذا الرابط "خصائص المواد الصلبة الأيونية https://ibb.co/ZRYj

--- src/app/materials/semester-1/unit-1/lesson-3/part-5/page.tsx
+++ src/app/materials/semester-1/unit-1/lesson-3/part-5/page.tsx
@@ -93,7 +93,7 @@ export default function LessonPartPage() {
                     <li><strong className="text-accent">لا توصل الكهرباء (صلبة)</strong> لأن أيوناتها مقيدة الحركة</li>
                     <li><strong className="text-accent">توصل الكهرباء (محلول/مصهور)</strong> لأن أيوناتها تصبح حرة الحركة</li>
                  </ul>
-                 <Image src="https://i.ibb.co/bgnDmdgw/2.png" alt="خصائص المواد الصلبة الأيونية" width={200} height={150} className="mx-auto" />
+                 <Image src="https://i.ibb.co/ZRYj1CS/2.png" alt="خصائص المواد الصلبة الأيونية" width={200} height={150} className="mx-auto" />
             </FlippableCard>
         </div>
         
@@ -139,6 +139,7 @@ export default function LessonPartPage() {
     </LessonLayout>
   );
 }
+
 
 
 
