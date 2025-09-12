42836ea41663386e072c7bf3eb18d826891ce13a
Hazim Jaber <h75jbr@gmail.com>
2025-09-12 09:19:36 +0000

    في السؤال
    استخدم الرابط
    <a href="https://ibb.co/Dfhp57HM"><img src="

--- src/app/materials/semester-1/unit-1/lesson-2/part-2/exam.tsx
+++ src/app/materials/semester-1/unit-1/lesson-2/part-2/exam.tsx
@@ -281,7 +281,7 @@ export const staticQuizLvl2: QuizQuestion[] = [
                 <p>اعتمادا على الرسم المجاور والمتعلق بالمواد <span dir="ltr" className="font-mono inline-block"><InlineMath math="CH_3CH_2OH"/></span>, <span dir="ltr" className="font-mono inline-block"><InlineMath math="CH_4"/></span>, <span dir="ltr" className="font-mono inline-block"><InlineMath math="CH_3CH_3"/></span>, <span dir="ltr" className="font-mono inline-block"><InlineMath math="CH_3Cl"/></span> فإن الرمز الذي يمثل الطاقة اللازمة لتبخر السائل <span dir="ltr" className="font-mono inline-block"><InlineMath math="CH_3Cl"/></span> هو:</p>
                 <div className="flex justify-center">
                     <Image
-                        src="https://i.ibb.co/hF9Fm0h/22.png"
+                        src="https://i.ibb.co/jkmRgLPw/3.jpg"
                         alt="Vapor Pressure vs Temperature for four liquids"
                         width={400}
                         height={250}
@@ -374,3 +374,6 @@ export const staticQuizLvl3: QuizQuestion[] = [
         explanation: "السائل الذي له أضعف قوى تجاذب هو الميثان (CH₄) لأنه غير قطبي وله أقل كتلة مولية. السائل ذو القوى الأضعف هو الأسرع تبخرًا والأعلى في الضغط البخاري، ويمثله المنحنى (C). الرمز (M) يمثل عدد الجسيئات التي تمتلك طاقة التبخر لهذا السائل، وهي أكبر كمية مقارنة بالسوائل الأخرى."
     }
 ];
+
+
+    
