7d85d607bdb9030033e4c196f08922a321e62dcb
Hazim Jaber <h75jbr@gmail.com>
2025-09-12 10:50:02 +0000

    في السؤال اعتمادا على الرسم المجاور، زيادة درجة حرارة منحنى د2 ستؤدي إلى

--- src/app/materials/semester-1/unit-1/lesson-2/part-2/exam.tsx
+++ src/app/materials/semester-1/unit-1/lesson-2/part-2/exam.tsx
@@ -39,7 +39,7 @@ export const staticQuizLvl1: QuizQuestion[] = [
                 <p>اعتمادا على الرسم المجاور فإن الرمز الذي يمثل الحد الأدنى من الطاقة اللازمة للتبخر هو:</p>
                 <div className="flex justify-center">
                     <Image
-                        src="https://i.ibb.co/v6zrQFzZ/22.png"
+                        src="https://i.ibb.co/MxNjGvNM/22.png"
                         alt="Maxwell-Boltzmann distribution curve"
                         width={400}
                         height={250}
@@ -236,7 +236,7 @@ export const staticQuizLvl2: QuizQuestion[] = [
                 <p>اعتمادا على الرسم المجاور، زيادة درجة حرارة منحنى د2 ستؤدي إلى الآتي:</p>
                 <div className="flex justify-center">
                     <Image
-                        src="https://i.ibb.co/ZJpPq6f/22.png"
+                        src="https://i.ibb.co/MxNjGvNM/22.png"
                         alt="مخطط توزيع ماكسويل-بولتزمان"
                         width={400}
                         height={250}
