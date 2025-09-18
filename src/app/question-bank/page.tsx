a1c7531b13076619293b01d2d6930fe48313c164
Hazim Jaber <h75jbr@gmail.com>
2025-09-18 14:07:13 +0000

    اربط اسئلة اختبار المحاليل السائلة بالبنك

--- src/app/question-bank/page.tsx
+++ src/app/question-bank/page.tsx
@@ -218,6 +218,7 @@ export default function QuestionBankPage() {
                 '/materials/semester-1/unit-1/section-5': () => import('@/app/materials/semester-1/unit-1/section-5/exam'),
                 '/materials/semester-1/unit-2/lesson-1/part-1': () => import('@/app/materials/semester-1/unit-2/lesson-1/part-1/exam'),
                 '/materials/semester-1/unit-2/lesson-1/part-2': () => import('@/app/materials/semester-1/unit-2/lesson-1/part-2/exam'),
+                '/materials/semester-1/unit-2/lesson-1/part-3': () => import('@/app/materials/semester-1/unit-2/lesson-1/part-3/exam'),
             };
 
             const loadedQuestions: SourcedQuizQuestion[] = [];
