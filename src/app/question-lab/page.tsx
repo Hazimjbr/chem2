c7756c4387b2f882a474482c6ad5a59299d3e17b
Hazim Jaber <h75jbr@gmail.com>
2025-09-18 15:16:46 +0000

    بس زبط طريقة عرض المركبات عشان تصير زي الصورة ويا ريت ما تتهبل وتجيب اس

--- src/app/question-lab/page.tsx
+++ src/app/question-lab/page.tsx
@@ -25,13 +25,13 @@ interface Question {
 const newQuestion: Question | null = {
     questionText: "المادة التي لا تذوب في الماء من المواد الآتية:",
     options: [
-        <span><InlineMath math="Na_2S" /></span>,
-        <span><InlineMath math="Ca(NO_3)_2" /></span>,
-        <span><InlineMath math="MgBr_2" /></span>,
-        <span><InlineMath math="CaCO_3" /></span>
+        <span dir="ltr"><InlineMath math="Na_2S" /></span>,
+        <span dir="ltr"><InlineMath math="Ca(NO_3)_2" /></span>,
+        <span dir="ltr"><InlineMath math="MgBr_2" /></span>,
+        <span dir="ltr"><InlineMath math="CaCO_3" /></span>
     ],
     correctAnswerIndex: 3,
-    explanation: "وفقًا لقواعد الذائبية، معظم الكربونات (CO₃²⁻) غير ذائبة في الماء، باستثناء كربونات الفلزات القلوية والأمونيوم. بينما أملاح النترات (NO₃⁻) والفلزات القلوية (Na⁺) والهاليدات (Br⁻) تكون ذائبة بشكل عام.",
+    explanation: "وفقًا لقواعد الذائبية، معظم الكربونات (CO₃²⁻) غير ذائبة في الماء باستثناء كربونات الفلزات القلوية والأمونيوم بينما أملاح النترات (NO₃⁻) والفلزات القلوية (Na⁺) والهاليدات (Br⁻) تكون ذائبة بشكل عام",
     level: 2,
     source: "الوحدة 2 / الدرس 1 / الجزء 3"
 };
